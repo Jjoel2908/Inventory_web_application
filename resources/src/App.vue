051c4f162234fb9cace4dbaea50eda7f6aa62977
Joel <joelalvaradom02@gmail.com>
2023-07-19 20:10:39 -0500

    first commit

---
+++ resources/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>LARAVEL CON VUE 3</h1>
+    <hr>
+    <h3> {{ title }} </h3>
+  </div>
+</template>
+
+<script setup>
+    const title = 'Laravel usando vue setup';
+</script>
+
+<style scoped>
+    div {
+        text-align: center;
+    }
+
+</style>
